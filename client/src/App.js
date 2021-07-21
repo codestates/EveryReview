d4275f188451ea66c3c921aece856647c269d3a0
ezyeon07 <ezyeon.july@gmail.com>
2021-07-22 01:20:14 +0900

    client/App.js-Modify-client automated deployment test

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Client Automated Deployment를 test 중입니다. 
         </a>
       </header>
     </div>
