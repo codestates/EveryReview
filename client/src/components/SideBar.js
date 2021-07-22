29b199e4e9929468b6e241241d6683c32bd95d6b
TaeGyeong1026 <ntk10912@gmail.com>
2021-07-22 16:49:14 +0900

    client-Home.js, Explore.js. App.js-Add-목업 관련 코드 및 파일 추가

--- client/src/components/SideBar.js
+++ client/src/components/SideBar.js
@@ -1,19 +1,29 @@
-
 import { Link } from 'react-router-dom';
+import './SideBar.css';
 
 function SideBar() {
   return (
-    <ul className="sidebar">
-      <li>
-        <Link to='/main/home'>Home</Link>
-      </li>
-      <li>
-        <Link to='/main/explore'>Explore</Link>
-      </li>
-      <li>
-        <Link to='/main/mypage'>mypage</Link>
-      </li>
-    </ul>
+    <section className="sideBar">
+      <div className="sideBarLogoWrap">
+        <div className="sideBarLogo">
+          <img src="#" alt="BI" />
+        </div>
+      </div>
+      <ul className="sideBarMenu">
+        <li className="sideBarMenu-li">
+          <Link to='/main/home'>Home</Link>
+        </li>
+        <li className="sideBarMenu-li">
+          <Link to='/main/explore'>#Explore</Link>
+        </li>
+        <li className="sideBarMenu-li">
+          <Link to='/main/mypage'>My Page</Link>
+        </li>
+      </ul>
+      <div className="logoutBtnWrap">
+        <button>로그아웃</button>
+      </div>
+    </section>
   );
 }
     
