c949ccfe183fc72f61c685b0c7fa8483a7796b96
kimeensung <kimeensung@gmail.com>
2021-07-29 14:07:59 +0900

    server-start.sh-fix-환경변수추가 설정

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,5 +6,7 @@ export DATABASE_USER=$(aws ssm get-parameters --region ap-northeast-2 --names DA
 export DATABASE_PASSWORD=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_PASSWORD --query Parameters[0].Value | sed 's/"//g')
 export DATABASE_NAME=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_NAME --query Parameters[0].Value | sed 's/"//g')
 export DATABASE_PORT=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_PORT --query Parameters[0].Value | sed 's/"//g')
+export KAKAO_CLIENT_ID=$(aws ssm get-parameters --region ap-northeast-2 --names KAKAO_CLIENT_ID --query Parameters[0].Value | sed 's/"//g')
+export KAKAO_REDIRECT_URI=$(aws ssm get-parameters --region ap-northeast-2 --names KAKAO_REDIRECT_URI --query Parameters[0].Value | sed 's/"//g')
 
 authbind --deep pm2 start index.js
